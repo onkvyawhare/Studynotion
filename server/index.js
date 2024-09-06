2dc855ccff762f9792225930efe116c1ab0a17a9
onkvyawhare <onkvyawhare@gmail.com>
2024-09-06 22:45:28 +0530

    fixed route error2

--- server/index.js
+++ server/index.js
@@ -24,7 +24,8 @@ app.use(cookieParser());
 app.use(
 	cors({
 		origin:"https://studynotion-7bnq.onrender.com",
-		credentials:true,
+		methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
+         allowedHeaders: 'Content-Type,Authorization'
 	})
 )
 
