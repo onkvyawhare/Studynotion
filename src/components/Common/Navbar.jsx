4991516b2486509c653adba7cf91cd2a4b98367c
onkvyawhare <onkvyawhare@gmail.com>
2024-09-07 17:54:55 +0530

    modified category schema

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -11,25 +11,6 @@ import { categories } from "../../services/apis"
 import { ACCOUNT_TYPE } from "../../utils/constants"
 import ProfileDropdown from "../core/Auth/ProfileDropdown"
 
-// const subLinks = [
-//   {
-//     title: "Python",
-//     link: "/catalog/python",
-//   },
-//   {
-//     title: "javascript",
-//     link: "/catalog/javascript",
-//   },
-//   {
-//     title: "web-development",
-//     link: "/catalog/web-development",
-//   },
-//   {
-//     title: "Android Development",
-//     link: "/catalog/Android Development",
-//   },
-// ];
-
 function Navbar() {
   const { token } = useSelector((state) => state.auth)
   const { user } = useSelector((state) => state.profile)
@@ -52,7 +33,7 @@ function Navbar() {
     })()
   }, [])
 
-  // console.log("sub links", subLinks)
+   console.log("sub links", subLinks)
 
   const matchRoute = (route) => {
     return matchPath({ path: route }, location.pathname)
@@ -89,7 +70,7 @@ function Navbar() {
                         <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
                         {loading ? (
                           <p className="text-center">Loading...</p>
-                        ) : subLinks.length ? (
+                        ) : (subLinks && subLinks.length) ? (
                           <>
                             {subLinks
                               ?.filter(
@@ -104,7 +85,7 @@ function Navbar() {
                                   className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
                                   key={i}
                                 >
-                                  <p>{subLink.name}</p>
+                                  <p className="text-black">{subLink.name}</p>
                                 </Link>
                               ))}
                           </>
