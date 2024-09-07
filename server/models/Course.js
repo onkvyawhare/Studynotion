29c265f8a86dee76b49d5f000e8b7c071de9d808
onkvyawhare <onkvyawhare@gmail.com>
2024-09-07 14:22:39 +0530

    modified course schema

--- server/models/Course.js
+++ server/models/Course.js
@@ -1,52 +1,63 @@
-const moongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const courseSchmea=new moongoose.Schema({
-    courseName:{
-        type:String,
-    },
-    courseDescription:{
-       
-        type:String,
-    },
-
-    instructor:{
-        type:moongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
-        
-
-
-    },
-    whatYouWillLearn:{
-        type:String,
-    },
-    courseContent:{
-        type:moongoose.Schema.Types.ObjectId,
-        ref:"Section",
-    },
-    ratingAndReviews:[{
-        type:moongoose.Schema.Types.ObjectId,
-        ref:"RatingAndReview"
-}],
-    price:{
-        type:Number,
-    },
-    thumbnail:{
-        type:String,
-    },
-    tag:{
-        type:moongoose.Schema.Types.ObjectId,
-        ref:"Tag"
-    },
-    studentsEnrolled:[{
-        type:moongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:"User",
-    }]
-
-    
-    
-   
+// Define the Courses schema
+const coursesSchema = new mongoose.Schema({
+	courseName: { type: String },
+	courseDescription: { type: String },
+	instructor: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "user",
+	},
+	whatYouWillLearn: {
+		type: String,
+	},
+	courseContent: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Section",
+		},
+	],
+	ratingAndReviews: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "RatingAndReview",
+		},
+	],
+	price: {
+		type: Number,
+	},
+	thumbnail: {
+		type: String,
+	},
+	tag: {
+		type: [String],
+		required: true,
+	},
+	category: {
+		type: mongoose.Schema.Types.ObjectId,
+		// required: true,
+		ref: "Category",
+	},
+	studentsEnrolled: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: "user",
+		},
+	],
+	instructions: {
+		type: [String],
+	},
+	status: {
+		type: String,
+		enum: ["Draft", "Published"],
+	},
+	createdAt: {
+		type:Date,
+		default:Date.now
+	},
 });
 
-module.exports=moongoose.model("Course",courseSchmea)
+// Export the Courses model
+module.exports = mongoose.model("Course", coursesSchema);
